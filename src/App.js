90b3c8ff33d6f790be3ae425624d3f1c5bb9ad1b
Ishan-Vimukthi <ishanvimukthie@gmail.com>
2024-02-27 01:01:08 +0530

    Finish front end ui part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Appbar from './components/AppBar.js';
+import Student from './components/Student.js';
 
 function App() {
   return (
     <div className="App">
       <Appbar/>
+      <Student/>
     </div>
   );
 }
--- src/components/Student.js
+++ src/components/Student.js
@@ -1,20 +1,44 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { Container, Paper, Button } from '@mui/material';
 
 export default function Student() {
+  const paperStyle={padding: '50px 20px', width:600, margin:'20px auto'}
+  const[name,setName] = useState('')
+  const[address,setAddress] = useState('')
+
+  const handleClick=(e)=>{
+    e.preventDefault()
+    const student = {name,address}
+    console.log(student)
+  }
+
   return (
-    <Box
+    <Container>    
+      <Paper elevation={3} style={paperStyle}>
+        <h1>Add Student</h1>
+      <Box
       component="form"
       sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
+        '& > :not(style)': { m: 1,  },
       }}
       noValidate
       autoComplete="off"
     >
       
-      <TextField id="standard-basic" label="Student Name" variant="standard" />
-      <TextField id="standard-basic" label="Student Adress" variant="standard" />
+      <TextField id="standard-basic" label="Student Name" variant="outlined" fullWidth
+        value={name}
+        onChange={(e)=>setName(e.target.value)}
+      />
+      <TextField id="standard-basic" label="Student Adress" variant="outlined" fullWidth
+        value={address}
+        onChange={(e)=>setAddress(e.target.value)}
+      />
+      <Button variant="contained" onClick={handleClick}>Submit</Button>
     </Box>
+    </Paper>
+    </Container>
+
   );
 }
